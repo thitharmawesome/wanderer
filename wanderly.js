02f70ef9b0b4d85133689baf4d7a0e772853ad37
Reema Thitha <reemathitha@Reemas-MacBook-Pro.local>
2016-12-05 13:38:31 -0500

    First commit

---
+++ wanderly.js
@@ -0,0 +1,13 @@
+function initMap() {
+  var uluru = {lat: 40.730, lng:-74};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
+
+ 
